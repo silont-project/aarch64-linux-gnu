48795415f69adf977f73c2299223a067bcb27b6d
Reinazhard <muh.alfarozy@gmail.com>
2021-01-20 20:16:08 +0530

    Update to https://github.com/gcc-mirror/gcc/commit/e1f4be24f46f33fa0399c3819835a98fd4b2322e, 20012021 build.
    GCC VERSION: aarch64-silont-linux-gnu-gcc (GCC) 10.2.1 20210120
    GCC COMMIT URL: https://github.com/gcc-mirror/gcc/commit/e1f4be24f46f33fa0399c3819835a98fd4b2322e
    BUILDER COMMIT: c43131fcb7527554e6d742f6fb6778c46535d2be

---
+++ lib/gcc/aarch64-silont-linux-gnu/10.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --enable-languages=c,c++ --target=aarch64-silont-linux-gnu --prefix=/drone/src/builder/aarch64-silont-linux-gnu --disable-multilib --disable-werror CFLAGS='-g0 -O3 -fstack-protector-strong' CXXFLAGS='-g0 -O3 -fstack-protector-strong'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
